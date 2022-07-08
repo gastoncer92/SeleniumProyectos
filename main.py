e8c7dd0091ad436c9a3a5d4a5440ebabaed38dfc
Gastón <gastongithub@gmail.com>
2022-07-08 03:03:37 -0300

    awdawdawd

--- main.py
+++ main.py
@@ -50,10 +50,44 @@ def insertar_base_grupo(conn, grupo):
     '''
 
 
-if __name__ == '__main__':
-    # inicio()
-    print("iniciando codigo")
+def guardar_elemento(indice):
+    pathNombreGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/span/div/a".format(
+        indice)
+    elem1 = driver.find_element(By.XPATH, pathNombreGrupo).text
+    pathDetalleGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/span/span".format(
+        indice)
+    elem2 = driver.find_element(By.XPATH, pathDetalleGrupo).text
+    print("Guardado:\n{}\n{}".format(elem1, elem2))
 
+
+def guardar_elemento2(indice):
+    pathNombreGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/span/div/a".format(indice)
+    elem1 = driver.find_element(By.XPATH, pathNombreGrupo).text
+    pathDetalleGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/span/span".format(indice)
+    elem2 = driver.find_element(By.XPATH, pathDetalleGrupo).text
+    print("Guardado:\n{}\n{}".format(elem1, elem2))
+    print('indice' + indice)
+
+
+def webScraper(indice):
+    articulo = driver.find_elements(By.XPATH, "//div[@role='article']")
+    try:
+        guardar_elemento(0)
+    except:
+        try:
+            guardar_elemento(1)
+            while articulo[indice]:
+                guardar_elemento(indice)
+                indice += 1
+        except:
+            driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+            try:
+                webScraper(indice)
+            except:
+                print("fin")
+
+
+if __name__ == '__main__':
     xpathMas = '//*[@id="mount_0_0_gB"]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[1]/ul/li[4]/span/div/a/span/svg'
     xpathGrupos = '//*[@id="mount_0_0_3W"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div[1]/ul/li[' \
                   '3]/div/a/div[2] '
@@ -62,6 +96,11 @@ if __name__ == '__main__':
     user = "gstncontacto@gmail.com"  # futura variable
     pwd = "Hm(Mb4c$4c27"  # futura variable
     botonGrupos = "//a[@role='link'][10]"
+    busqueda1 = 'Compra Venta'
+    busqueda2 = 'vfbawdawddawgnz'
+    busqueda3 = 'zxzxzzxzxzx'
+    busqueda4 = 'lllllllllllllllllllllllllllllllllllllllllllllll'
+    busqueda = busqueda4
     elem = driver.find_element(By.ID, "email")
     elem.send_keys(user)
     driver.implicitly_wait(3)
@@ -71,62 +110,34 @@ if __name__ == '__main__':
     driver.implicitly_wait(2)
     driver.find_element(By.XPATH, "//a[@aria-label='Grupos']").click()
     driver.implicitly_wait(2)
-    driver.find_element(By.XPATH, "//input[@aria-label='Buscar grupos']").send_keys('Compra Venta', Keys.ENTER)  #
+    driver.find_element(By.XPATH, "//input[@aria-label='Buscar grupos']").send_keys(busqueda, Keys.ENTER)  #
     driver.implicitly_wait(2)
-    driver.find_element(By.XPATH, "//input[@aria-label='Buscar grupos']").send_keys('Compra Venta', Keys.ESCAPE)  #
-    # elem = driver.find_elements(By.XPATH, "//div[@role='listitem']/div[@data-visualcompletion='ignore-dynamic']/a")[1].click()
-
-    # elem = driver.find_elements(By.XPATH, "//div[@role='listitem']/div/a[@role='link']")[1].click()
+    driver.find_element(By.XPATH, "//input[@aria-label='Buscar grupos']").send_keys(busqueda, Keys.ESCAPE)  #
     driver.find_elements(By.XPATH, "//div[@role='listitem']/div/a[@role='link']")[1].click()
 
-    # $x("//div[@role='listitem']/div/a[@role='link']")[1] boton grupos
+    indice = 1
+    # cantidad = len(driver.find_elements(By.XPATH, "//div[@role='article']"))
 
-    # elem = driver.find_elements(By.XPATH, "//div[@role='article']")[0]
-    # $x("//div[@role='article'][1]//div[@class='qzhwtbm6 knvmm38d']//a")
-    # elem1 = driver.find_element(By.XPATH, "//div[@role='article'][0]//div[@class='qzhwtbm6 knvmm38d'][0]//a").text
-    elem1 = driver.find_element(By.XPATH,
-                                "//div[@role='article'][1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/span/div/a").text
-    print(elem1)
 
-    elem2 = driver.find_element(By.XPATH,
-                                "//div[@role='article'][1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/span/span").text
-    print(elem2)
+    while indice <= len(driver.find_elements(By.XPATH, "//div[@role='article']")):
+        print('----------------------')
+        guardar_elemento2(indice)
+        indice += 1
+    else:
+        driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+    driver.quit()
 
-    cantidadElementos = len(driver.find_elements(By.XPATH, "//div[@role='article']"))
-    print(cantidadElementos)
-
-    for i in list(range(1, cantidadElementos + 1)):
-        print(i)
-        pathNombreGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/span/div/a".format(i)
-        print(pathNombreGrupo)
-        elem1 = driver.find_element(By.XPATH,pathNombreGrupo).text
-        print(elem1)
-        pathDetalleGrupo = "//div[@role='article'][{}]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/span/span".format(i)
-        print(elem2)
-
-    #driver.quit()
-
-    # $x("//div[@role='article'][1]//div[@class='qzhwtbm6 knvmm38d'][2]/span")
-    # elem2 = driver.find_element(By.XPATH, "//div[@role='article'][0]//div[@class='qzhwtbm6 knvmm38d'][1]/span").text
-    # elem1=elem.find_elements(By.XPATH,"//div[@role='article'][1]//div[@class='qzhwtbm6 knvmm38d'][1]")
-    # elem2=elem.find_elements(By.XPATH,"//div[@class='qzhwtbm6 knvmm38d']")[2].text
-
-    # print(elem2)
-
-    # elem[1].click()
-    # driver.implicitly_wait(5)
     #
-    # elem=driver.find_elements(By.XPATH,"//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gpro0wi8 oo9gr5id lrazzd5p']")
     # try:
-    #
-    #     for i in elem:
-    #         driver.implicitly_wait(5)
-    #         elem1 = i.text
-    #
-    #         print(elem1)
-    #
+    #     elem1 = driver.find_element(By.XPATH,
+    #                                 "//div[@role='article'][1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[1]/span/div/a").text
+    #     elem2 = driver.find_element(By.XPATH,
+    #                                 "//div[@role='article'][1]/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/span/span").text
+    #     articulo = driver.find_elements(By.XPATH, "//div[@role='article']")
+    #     guardar_elemento(0)
+    #     indice = 2
+    #     webScraper(indice)
+    #     driver.quit()
     # except:
-    #     print('error')
-    #
-    # print('fin')
-    # driver.quit()
+    #     print('fin innesperado')
+    #     driver.quit()
